7493a54137749ed48d53a900194494ad1c8a44d6
peacedude <victor_sanden@hotmail.com>
2017-03-29 13:38:41 +0200

    Add project files.

---
+++ mvcLayoutViews/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcLayoutViews.Models;
+
+namespace mvcLayoutViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Victor";
+            model.Location = "Skövde";
+            return View(model);
+        }
+    }
+}
